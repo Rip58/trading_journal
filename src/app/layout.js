e73cd45aade752a208c6738ff1f0b165fa1a2922
Rip58 <sergibenamor@me.com>
2026-05-28 12:24:30 +0200

    Fix: suppress hydration warning on root html element for theme class modification

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="es">
+    <html lang="es" suppressHydrationWarning>
       <head>
         <script
           dangerouslySetInnerHTML={{
